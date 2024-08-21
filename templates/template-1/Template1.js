07626aad80d0bde61c194ca8dbd8653e02d5771f
Saikat LiftOff <60886046+saikat-liftoffllc@users.noreply.github.com>
2024-08-21 15:44:34 +0530

    Update

--- templates/template-1/Template1.js
+++ templates/template-1/Template1.js
@@ -2,10 +2,57 @@ class Template1 extends AbstractTemplate {
   constructor(containerElemId) {
     super(containerElemId);
     this.contentUri = "/templates/template-1/content.html";
+    this.rootElem = $(this.shadowRoot);
+    this.rendered = false;
   }
 
   render() {
     super.render();
-    // TODO: Render data
+    this.rendered = true;
+  }
+
+  renderData(data) {
+    if (!this.rendered) {
+      console.log("Render the Template first");
+      return;
+    }
+
+    // Render data
+    const fields = this.getFields();
+    fields.forEach((field) => {
+      const value = data[field] || "";
+      console.log(`Rendering data ${field} with ${value}`);
+      this.setField(field, value);
+    });
+  }
+
+  setField(field, data) {
+    this.rootElem.find(`.template-content #${field} .data`).text(data);
+  }
+
+  getFields() {
+    return [
+      "address",
+      "receiptNo",
+      "localId",
+      "fipNo",
+      "nozzelNo",
+      "product",
+      "presetType",
+      "rate",
+      "volume",
+      "atot",
+      "vtot",
+      "vehicleNo",
+      "mobileNo",
+      "date",
+      "time",
+      "cstNo",
+      "lstNo",
+      "vatNo",
+      "attendantId",
+      "fccDate",
+      "fccTime",
+    ];
   }
 }
