a27af3d89aefd90e33feed25abd4c5ad800418c1
Saikat LiftOff <60886046+saikat-liftoffllc@users.noreply.github.com>
2024-08-25 12:50:56 +0530

    Update

---
+++ templates/template-2/Template2.js
@@ -0,0 +1,8 @@
+class Template2 extends AbstractTemplate {
+  constructor(containerElemId) {
+    super(containerElemId);
+    this.contentUri = "templates/template-2/content.html";
+  }
+
+  renderData(data) {}
+}
